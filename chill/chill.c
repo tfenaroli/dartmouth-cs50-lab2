6d3fa845476378987ae1f0857641923fb2a98096
Thomas S. Fenaroli <f004y3m@plank.thayer.dartmouth.edu>
2021-09-27 15:23:53 -0400

    finished chill.c and testing script

---
+++ chill/chill.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+double calculateWindChill(double temp, double windVelocity) {
+  double result = 35.74 + 0.6215*temp - 35.75*pow(windVelocity,0.16) + 0.4275*temp*pow(windVelocity,0.16);
+  return result;
+}
+
+int main( int argc, char *argv[] ) {
+
+  if (0<argc && argc <4) {
+
+    if (argc==1) {
+
+      printf("  Temp   Wind  Chill\n");
+      printf("  ----   ----   ----\n");
+
+      double temps[6] = {-10.0, 0.0, 10.0, 20.0, 30.0, 40.0};
+      double winds[3] = {5.0, 10.0, 15.0};
+      for (int i=0; i<6; i++) {
+        for (int j=0; j<3; j++) {
+          double chill = calculateWindChill(temps[i], winds[j]);
+          printf("%6.1f %6.1f %6.1f\n", temps[i], winds[j], chill);
+        }
+      }
+    }
+    else if (argc==2) {
+
+      double temp = strtod(argv[1], NULL);
+      
+      if (temp <= -99.0 || temp >= 50.0) {
+        printf("Temperature must be between -99 and 50 degrees!\n");
+        return 1;
+      }
+
+      printf("  Temp   Wind  Chill\n");
+      printf("  ----   ----   ----\n");
+
+      double winds[3] = {5.0, 10.0, 15.0};
+      for (int i=0; i<3; i++) {
+          double chill = calculateWindChill(temp, winds[i]);
+          printf("%6.1f %6.1f %6.1f\n", temp, winds[i], chill);
+        }
+    }
+    else {
+
+      double temp = strtod(argv[1], NULL);
+      double wind = strtod(argv[2], NULL);
+
+      if ((temp <= -99.0 || temp >= 50.0) || (wind < 0.5)) {
+        printf("Temperature must be between -99 and 50 degrees and wind velocity must be greater than 0.5 mph!\n");
+        return 1;
+      }
+
+      printf("  Temp   Wind  Chill\n");
+      printf("  ----   ----   ----\n");
+
+      double chill = calculateWindChill(temp, wind);
+      printf("%6.1f %6.1f %6.1f\n", temp, wind, chill);
+    }
+  }
+
+  else {
+    printf("Improper number of arguments!");
+  }
+  
+  return 0;
+}
---
+++ chill/testing.sh
@@ -0,0 +1,20 @@
+## testing script
+
+# basic test
+./chill
+
+# testing (with proper argument) one argument
+./chill 10.0
+
+# testing (with improper argument) one argument
+./chill 100.0
+
+# testing (with proper arguments) two arguments
+./chill 5.0 5.0
+
+# testing (with improper arguments) two arguments
+./chill 20.0 0.0
+
+# testing improper number of arguments
+./chill 1.0 2.0 3.0 4.0
+
